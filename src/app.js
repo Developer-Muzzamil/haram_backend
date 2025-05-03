058e0ad28ad55251c4c46ec7d18428d22e1fc8ad
Developer Muzzamil <developermuzzamil@gmail.com>
2025-05-03 07:03:57 +0530

    prefinal commit fixed coros errors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const enquiryRoutes = require("./routes/enquiryRoutes");
 const app = express();
 
 app.use(cors({
-  origin: 'https://nextstop-haramain.netlify.app/' // or use '*' for development
+  origin: 'https://nextstop-haramain.netlify.app' // or use '*' for development
 }));
 
 app.use(express.json());
