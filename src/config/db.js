333913a3a5959f794f87cb719ec64a5981b6be38
mrcartoony315 <khansufyaan315@gmail.com>
2025-05-31 11:58:04 +0530

    change

--- src/config/db.js
+++ src/config/db.js
@@ -10,9 +10,7 @@ const connectDB = async () => {
   }
 };
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'));
+mongoose.connect(process.env.MONGO_URI,)
+.then(() => console.log('MongoDB connected'));
 
 module.exports = connectDB;
